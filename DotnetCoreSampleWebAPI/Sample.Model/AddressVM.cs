a72f414f66f973d22fd99cbe45ca17d5cd8fb849
mohamedaashiq10665 <aashiqmohameda@gmail.com>
2021-06-24 23:58:41 +0530

    Added View Models

---
+++ DotnetCoreSampleWebAPI/Sample.Model/AddressVM.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Sample.Util;
+using System;
+using System.Collections.Generic;
+
+namespace Sample.Model
+{
+    public class AddressVM
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("street")]
+        public string street { get; set; }
+        [JsonProperty("city")]
+        public string city { get; set; }
+        [JsonProperty("postal_code")]
+        public int postal_code { get; set; }
+
+        public void NullOrEmptyValidation(IList<AppError> errors)
+        {
+            if (String.IsNullOrEmpty(street))
+            {
+                AppError.STREET_NOT_PROVIDED.message = "Field street is required. Please provide a value for field street";
+                errors.Add(AppError.STREET_NOT_PROVIDED);
+            }
+
+            if (String.IsNullOrEmpty(city))
+            {
+                AppError.CITY_NOT_PROVIDED.message = "Field city is required. Please provide a value for field city";
+                errors.Add(AppError.CITY_NOT_PROVIDED);
+            }
+
+            if (postal_code == 0)
+            {
+                AppError.POSTAL_CODE_NOT_PROVIDED.message = "Field postal_code is required. Please provide a value for field postal_code";
+                errors.Add(AppError.POSTAL_CODE_NOT_PROVIDED);
+            }
+        }
+    }
+}
---
+++ DotnetCoreSampleWebAPI/Sample.Model/PersonVM.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Sample.Util;
+using System;
+using System.Collections.Generic;
+
+namespace Sample.Model
+{
+    public class PersonVM
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("address")]
+        public AddressVM address { get; set; }
+
+        public void NullOrEmptyValidation()
+        {
+            IList<AppError> errors = new List<AppError>();
+            if (String.IsNullOrEmpty(name))
+            {
+                AppError.NAME_NOT_PROVIDED.message = "Field name is required. Please provide a value for field name";
+                errors.Add(AppError.NAME_NOT_PROVIDED);
+            }
+            address.NullOrEmptyValidation(errors);
+
+            if (errors.Count != 0)
+                throw new AppException(errors);
+        }
+
+    }
+}
