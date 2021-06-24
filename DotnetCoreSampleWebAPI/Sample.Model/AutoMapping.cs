6e9236c6bd5529e1b0cf0facec4d41a209502696
mohamedaashiq10665 <aashiqmohameda@gmail.com>
2021-06-25 00:00:22 +0530

    Added AutoMapper Class

---
+++ DotnetCoreSampleWebAPI/Sample.Model/AutoMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Sample.Dto;
+
+namespace Sample.Model
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<PersonVM, Person>();
+            CreateMap<Person, PersonVM>();
+            CreateMap<AddressVM, Address>();
+            CreateMap<Address, AddressVM>();
+        }
+    }
+}
